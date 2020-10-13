1268cfff9e7908b02a907c7e566a967f7a3cf4b7
Juergen E. Fischer <jef@norbit.de>
2020-10-13 22:31:32 +0200

    swig: add library files

--- src/swig/osgeo4w/package.sh
+++ src/swig/osgeo4w/package.sh
@@ -8,19 +8,29 @@ source ../../../scripts/build-helpers
 
 startlog
 
-[ -f swigwin-4.0.2.zip ] || wget http://prdownloads.sourceforge.net/swig/swigwin-$V.tar.gz
-[ -f ../swigwin-$V/swig.exe ] || unzip -d .. swigwin-$V.zip swigwin-$V/LICENSE swigwin-$V/swig.exe
+[ -f swigwin-4.0.2.zip ] || wget http://prdownloads.sourceforge.net/swig/swigwin-$V.zip
+[ -f ../swigwin-$V/swig.exe ] || unzip -d .. swigwin-$V.zip
 
 export R=$OSGEO4W_REP/x86_64/release/$P
 mkdir -p $R
 
-tar -C ../swigwin-$V -cjf $R/$P-$V-$B.tar.bz2 \
-	--xform "s,swig.exe,bin/swig.exe," \
-	swig.exe
+cat <<EOF >swig.bat
+@"%OSGEO4W_ROOT%\\apps\\swigwin\\swig.exe" %*
+EOF
+
+tar -cjf $R/$P-$V-$B.tar.bz2 \
+	--exclude "../swigwin-$V/Examples" \
+	--exclude "../swigwin-$V/Source" \
+	--exclude "../swigwin-$V/Tools" \
+	--exclude "../swigwin-$V/CCache" \
+	--xform "s,swigwin-$V,apps/swigwin," \
+	--xform "s,swig.bat,bin/swig.bat," \
+	../swigwin-$V \
+	swig.bat
 
 cat <<EOF >$R/setup.hint
-sdesc: "SWIG is a tool for build language bindings."
-ldesc: "SWIG is a tool for build language bindings.
+sdesc: "SWIG is a tool to build language bindings."
+ldesc: "SWIG is a tool to build language bindings.
 Repackaged binary from swig.org
 "
 category: Commandline_Utilities
