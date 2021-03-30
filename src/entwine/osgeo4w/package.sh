69adb4eec222af651b6cfe0e3ffd3b2d76f9afb7
Juergen E. Fischer <jef@norbit.de>
2021-03-30 20:33:33 +0200

    include entwine

---
+++ src/entwine/osgeo4w/package.sh
@@ -0,0 +1,68 @@
+export P=entwine
+export V=2.1.0
+export B=next
+export MAINTAINER=JuergenFischer
+export BUILDDEPENDS="curl-devel openssl-devel pdal-devel python3-core"
+
+source ../../../scripts/build-helpers
+
+startlog
+
+[ -f $V.tar.gz ] || wget -q https://github.com/connormanning/$P/archive/$V.tar.gz
+[ -d ../$P-$V/CMakeLists.txt ] || tar -C .. -xzf $V.tar.gz
+
+vs2019env
+cmakeenv
+ninjaenv
+
+mkdir -p build install
+cd build
+
+export INCLUDE="$INCLUDE;$(cygpath -am ../osgeo4w/include)"
+export LIB="$LIB;$(cygpath -am ../osgeo4w/lib)"
+
+cmake -Wno-dev -G Ninja \
+	-D CMAKE_BUILD_TYPE=Release \
+	-D CMAKE_INSTALL_PREFIX=../install \
+	-D CMAKE_PREFIX_PATH=$(cygpath -am ../osgeo4w/lib/cmake/PDAL) \
+	-D BUILD_SHARED_LIBS=ON \
+	-D PYTHON_EXECUTABLE=$(cygpath -am ../osgeo4w/bin/python3.exe) \
+	-D CMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING \
+	../../$P-$V
+ninja
+ninja install
+
+cd ..
+
+export R=$OSGEO4W_REP/x86_64/release/$P
+mkdir -p $R/$P-devel
+
+cat <<EOF >$R/setup.hint
+sdesc: "data organization library for massive point clouds (Runtime)"
+ldesc: "Entwine is a data organization library for massive point clouds"
+category: CommandLine_Utilities
+requires: msvcrt2019 $RUNTIMEDEPENDS pdal-libs openssl curl
+maintainer: $MAINTAINER
+EOF
+
+tar -C install -cjf $R/$P-$V-$B.tar.bz2 bin/entwine.exe
+
+cat <<EOF >$R/$P-devel/setup.hint
+sdesc: "data organization library for massive point clouds (Development)"
+ldesc: "Entwine is a data organization library for massive point clouds"
+category: Libs
+requires: $P
+external-source: $P
+maintainer: $MAINTAINER
+EOF
+
+tar -C install -cjf $R/$P-devel/$P-devel-$V-$B.tar.bz2 \
+	include \
+	lib
+
+cp ../$P-$V/LICENSE $R/$P-$V-$B.txt
+cp ../$P-$V/LICENSE $R/$P-devel/$P-devel-$P-$V-$B.txt
+
+tar -C .. -cjf $R/$P-$V-$B-src.tar.bz2 osgeo4w/package.sh
+
+endlog
