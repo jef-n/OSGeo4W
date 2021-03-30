3c676d1de569ef969afa32faea851556d4b5bd22
Juergen E. Fischer <jef@norbit.de>
2021-03-30 11:27:05 +0200

    fix signing of msis

--- scripts/msis.sh
+++ scripts/msis.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cert=src/setup/osgeo4w/OSGeo_DigiCert_Signing_Cert
+cert=$PWD/src/setup/osgeo4w/OSGeo_DigiCert_Signing_Cert
 
 [ -r "$cert.p12" ]
 [ -r "$cert.pass" ]
@@ -11,11 +11,11 @@ for i in qgis qgis-ltr; do
 	releasename=$(sed -ne 's/^set(RELEASE_NAME "\(.*\)").*$/\1/ip' src/$i/qgis/CMakeLists.txt)
 
 	perl scripts/createmsi.pl \
-		-sign-with=$cert.p12 \
+		-signwith=$cert.p12 \
 		-signpass=$(<$cert.pass) \
 		-verbose \
 		-releasename="$releasename" \
-		-banner=src/$i/osgeo4w/qgis_msibanner.bmp \
-		-background=src/$i/osgeo4w/qgis_msiinstaller.bmp \
+		-banner=$PWD/src/$i/osgeo4w/qgis_msibanner.bmp \
+		-background=$PWD/src/$i/osgeo4w/qgis_msiinstaller.bmp \
 		$i-full
 done
