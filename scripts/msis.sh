c7fcd318093fec0a203914b629db1fc32ee562c6
Juergen E. Fischer <jef@norbit.de>
2021-04-17 17:08:58 +0200

    msis.sh: set shortname

--- scripts/msis.sh
+++ scripts/msis.sh
@@ -7,7 +7,7 @@ cert=$PWD/src/setup/osgeo4w/OSGeo_DigiCert_Signing_Cert
 [ -r "$cert.p12" ]
 [ -r "$cert.pass" ]
 
-for i in qgis qgis-ltr; do
+for i in ${PKGS:-qgis qgis-ltr}; do
 	releasename=$(sed -ne 's/^set(RELEASE_NAME "\(.*\)").*$/\1/ip' src/$i/qgis/CMakeLists.txt)
 
 	perl scripts/createmsi.pl \
@@ -15,6 +15,7 @@ for i in qgis qgis-ltr; do
 		-signpass=$(<$cert.pass) \
 		-verbose \
 		-releasename="$releasename" \
+		-shortname="$i" \
 		-banner=$PWD/src/$i/osgeo4w/qgis_msibanner.bmp \
 		-background=$PWD/src/$i/osgeo4w/qgis_msiinstaller.bmp \
 		$i-full
