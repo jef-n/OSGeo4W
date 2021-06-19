8b2c6a85c1785a2f73e15a6fc402074a2e1e2146
Juergen E. Fischer <jef@norbit.de>
2021-06-19 12:30:21 +0200

    introduce empty shell package to notify about unsupported upgrades from old osgeo4w

---
+++ src/shell/osgeo4w/package.sh
@@ -0,0 +1,38 @@
+export P=shell
+export V=99.0.0
+export B=next
+export MAINTAINER=JuergenFischer
+export BUILDDEPENDS=none
+
+source ../../../scripts/build-helpers
+
+startlog
+
+export R=$OSGEO4W_REP/x86_64/release/$P
+mkdir -p $R
+
+cat <<EOF >$R/setup.hint
+sdesc: "UPGRADES FROM OLD OSGEO4W NOT SUPPORTED"
+ldesc: "OSGeo4W transitional package
+
+UPGRADES FROM OLD OSGEO4W NOT SUPPORTED"
+maintainer: $MAINTAINER
+category: Commandline_Utilities
+requires:
+EOF
+
+
+cat <<EOF >postinstall.bat
+msg "*" UPGRADES FROM OLD OSGEO4W NOT SUPPORTED - PLEASE DO A FRESH INSTALL
+echo UPGRADES FROM OLD OSGEO4W NOT SUPPORTED - PLEASE DO A FRESH INSTALL
+exit /b 1
+EOF
+
+tar -cjf $R/$P-$V-$B.tar.bz2 \
+	--xform "s,^postinstall.bat,etc/postinstall/$P.bat," \
+	postinstall.bat
+
+tar -C .. -cjf $R/$P-$V-$B-src.tar.bz2 \
+	osgeo4w/package.sh
+
+endlog
