cfc7937f991dd40cbc89ee791ccdffd424001fcd
Juergen E. Fischer <jef@norbit.de>
2021-11-18 01:18:57 +0100

    skip msi signing if certs are not available

--- scripts/msis.sh
+++ scripts/msis.sh
@@ -4,15 +4,15 @@ set -e
 
 cert=$PWD/src/setup/osgeo4w/OSGeo_DigiCert_Signing_Cert
 
-[ -r "$cert.p12" ]
-[ -r "$cert.pass" ]
+if [ -r "$cert.p12" -a -r "$cert.pass" ]; then
+	sign="-signwith=$cert.p12 -signpass=$(<$cert.pass)"
+fi
 
 for i in ${PKGS:-qgis qgis-ltr}; do
 	releasename=$(sed -ne 's/^set(RELEASE_NAME "\(.*\)").*$/\1/ip' src/$i/qgis/CMakeLists.txt)
 
 	perl scripts/createmsi.pl \
-		-signwith=$cert.p12 \
-		-signpass=$(<$cert.pass) \
+		$sign \
 		-verbose \
 		-releasename="$releasename" \
 		-shortname="$i" \
