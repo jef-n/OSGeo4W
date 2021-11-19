6fec4f54f64d388137c782e6d1a2c03bdf342b37
Juergen E. Fischer <jef@norbit.de>
2021-11-19 09:33:51 +0100

    update pcraster to 4.3.2

--- src/pcraster/osgeo4w/package.sh
+++ src/pcraster/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=pcraster
-export V=4.3.1
+export V=4.3.2
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="gdal gdal-devel python3-core python3-devel python3-numpy python3-pybind11 xerces-c-devel qt5-devel qt5-oci qt5-oci-debug boost-devel"
@@ -13,20 +13,20 @@ if [ -d ../$P-$V ]; then
 	git pull
 	git submodule update --recursive
 else
+	rm -fr build
 	git clone --recurse-submodules https://github.com/$P/$P.git --branch v$V --single-branch ../$P-$V
 	cd ../$P-$V
 fi
 
-cd devbase
-git reset --hard
-patch -p1 --dry-run <../../osgeo4w/devbase.patch
-patch -p1 <../../osgeo4w/devbase.patch
-cd ../../osgeo4w
+cd ../osgeo4w
 
 (
+	set -e
+
 	fetchenv osgeo4w/bin/o4w_env.bat
 	vs2019env
 	cmakeenv
+	ninjaenv
 
 	mkdir -p install build
 
@@ -45,30 +45,56 @@ cd ../../osgeo4w
 
 	export PATH="$(cygpath -a ../osgeo4w/bin):$(cygpath -a ../osgeo4w/apps/qt5/bin):$PATH"
 
-	cmake -G Ninja \
-		-Wno-dev \
-		-D CMAKE_MODULE_PATH=$(cygpath -am ../osgeo4w/share/cmake) \
-		-D CMAKE_PREFIX_PATH=$(cygpath -am ../osgeo4w/apps/qt5/lib/cmake) \
-		-D CMAKE_TOOLCHAIN_FILE=$(cygpath -am ../msvs2019.cmake) \
-		-D Boost_USE_STATIC_LIBS=ON \
-		-D Boost_USE_MULTITHREADED=ON \
-		-D Boost_USE_STATIC_RUNTIME=OFF \
-		-D Boost_INCLUDE_DIR="$(cygpath -am ../osgeo4w/include/boost-1_74)" \
-		-D Boost_LIBRARY_DIR="$(cygpath -am ../osgeo4w/lib)" \
-		-D CMAKE_CXX_STANDARD=17 \
-		-D CMAKE_BUILD_TYPE=Release \
-		-D CMAKE_INSTALL_PREFIX=../install \
-		-D PCRASTER_PYTHON_INSTALL_DIR=$(cygpath -am ../install/apps/Python39/Lib/site-packages) \
-		-D XercesC_INCLUDE_DIR=$(cygpath -am ../osgeo4w/include) \
-		-D XercesC_LIBRARY=$(cygpath -am ../osgeo4w/lib/xerces-c_3.lib) \
-		-D Python3_EXECUTABLE=$(cygpath -am ../osgeo4w/bin/python.exe) \
-		-D pybind11_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/share/cmake/pybind11) \
-		-D PYBIND11_SYSTEM_INCLUDE=$(cygpath -aw ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/include) \
-		-D Python3_NumPy_INCLUDE_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/numpy/core/include) \
-		-D GDAL_DATA=$(cygpath -am ../osgeo4w/share/gdal) \
-		-D Qt5_DIR=$(cygpath -am ../osgeo4w/apps/Qt5) \
-		-D PCRASTER_BUILD_TEST=OFF \
-        	../../$P-$V
+	while :; do
+		if ! cmake -G Ninja \
+			-Wno-dev \
+			-D CMAKE_MODULE_PATH=$(cygpath -am ../osgeo4w/share/cmake) \
+			-D CMAKE_PREFIX_PATH=$(cygpath -am ../osgeo4w/apps/qt5/lib/cmake) \
+			-D CMAKE_TOOLCHAIN_FILE=$(cygpath -am ../msvs2019.cmake) \
+			-D Boost_USE_STATIC_LIBS=ON \
+			-D Boost_USE_MULTITHREADED=ON \
+			-D Boost_USE_STATIC_RUNTIME=OFF \
+			-D Boost_INCLUDE_DIR="$(cygpath -am ../osgeo4w/include/boost-1_74)" \
+			-D Boost_LIBRARY_DIR="$(cygpath -am ../osgeo4w/lib)" \
+			-D CMAKE_CXX_STANDARD=17 \
+			-D CMAKE_BUILD_TYPE=Release \
+			-D CMAKE_INSTALL_PREFIX=../install \
+			-D PCRASTER_PYTHON_INSTALL_DIR=$(cygpath -am ../install/apps/Python39/Lib/site-packages) \
+			-D XercesC_INCLUDE_DIR=$(cygpath -am ../osgeo4w/include) \
+			-D XercesC_LIBRARY=$(cygpath -am ../osgeo4w/lib/xerces-c_3.lib) \
+			-D Python3_EXECUTABLE=$(cygpath -am ../osgeo4w/bin/python.exe) \
+			-D pybind11_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/share/cmake/pybind11) \
+			-D PYBIND11_SYSTEM_INCLUDE=$(cygpath -aw ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/include) \
+			-D Python3_NumPy_INCLUDE_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/numpy/core/include) \
+			-D GDAL_DATA=$(cygpath -am ../osgeo4w/share/gdal) \
+			-D Qt5_DIR=$(cygpath -am ../osgeo4w/apps/Qt5) \
+			-D PCRASTER_BUILD_TEST=OFF \
+			../../$P-$V; then
+
+			patched=0
+			for devbase in _deps/devbase-src _deps/fern-src/devbase; do
+				[ -d $devbase ]
+
+				if ! [ -f $devbase/patched ]; then
+					patch --dry-run -p1 -d $devbase <../devbase.patch
+					patch           -p1 -d $devbase <../devbase.patch
+					touch $devbase/patched
+				fi
+
+				(( patched++ )) || true
+			done
+
+			if (( patched < 2 )); then
+				exit 1
+			fi
+
+			continue
+		fi
+
+		touch ../../$P-$V/CMakeLists.txt
+		break
+	done
+
 	ninja
 	ninja install
 )
