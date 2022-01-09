426e992b71ae7cc44c427e255b77d15776cc355c
Juergen E. Fischer <jef@norbit.de>
2022-01-09 21:17:39 +0100

    update matplotlib to 3.5.1 and include fonttools

--- src/python3-matplotlib/osgeo4w/package.sh
+++ src/python3-matplotlib/osgeo4w/package.sh
@@ -2,7 +2,7 @@ export P=python3-matplotlib
 export V=pip
 export B=pip
 export MAINTAINER=JuergenFischer
-export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-pyparsing python3-numpy python3-cycler python3-kiwisolver python3-python-dateutil"
+export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-pyparsing python3-numpy python3-cycler python3-kiwisolver python3-python-dateutil python3-fonttools"
 
 source ../../../scripts/build-helpers
 
