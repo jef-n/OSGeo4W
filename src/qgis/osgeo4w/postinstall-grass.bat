4c38a12841e8ac4c471ad7762cafcfe4c4c70748
Juergen E. Fischer <jef@norbit.de>
2022-03-18 18:52:15 +0100

    qgis{,-ltr}: also update .env when grass plugin is installed

--- src/qgis/osgeo4w/postinstall-grass.bat
+++ src/qgis/osgeo4w/postinstall-grass.bat
@@ -1,3 +1,6 @@
 call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
+set APPNAME=QGIS Desktop @version@
+for %%i in ("%OSGEO4W_STARTMENU%") do set QGIS_WIN_APP_NAME=%%~ni\%APPNAME%
+call "%OSGEO4W_ROOT%\bin\@package@.bat" --postinstall
 del /s /q "%OSGEO4W_ROOT%\apps\@package@\grass\*.pyc"
 exit /b 0
