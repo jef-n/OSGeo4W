6a51a5422c53117c68e2bb3a9241deb959175d62
Juergen E. Fischer <jef@norbit.de>
2022-06-16 11:51:46 +0200

    libpq: update to 14.3 (fixes #747; also include pg_dump/pg_restore)

--- src/libpq/osgeo4w/package.sh
+++ src/libpq/osgeo4w/package.sh
@@ -1,18 +1,18 @@
 export P=libpq
-export V=13.0
+export V=14.3
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="openssl-devel zlib-devel libiconv-devel"
 
 # perl also used in openssl and qt5
-SBPERL=5.32.0.1
+SBPERL=5.32.1.1
 
 source ../../../scripts/build-helpers
 
 startlog
 
 [ -f postgresql-$V.tar.bz2 ] || wget https://ftp.postgresql.org/pub/source/v$V/postgresql-$V.tar.bz2
-[ -f ../Makefile ] || tar -C .. -xjf postgresql-$V.tar.bz2 --xform "s,postgresql-$V,.,"
+[ -f ../postgresql-$V/Makefile ] || tar -C .. -xjf postgresql-$V.tar.bz2
 
 if ! [ -d perl ]; then
 	wget -c http://strawberryperl.com/download/$SBPERL/strawberry-perl-$SBPERL-64bit-portable.zip
@@ -22,7 +22,7 @@ if ! [ -d perl ]; then
 	cd ..
 fi
 
-cat <<EOF >../src/tools/msvc/config.pl
+cat <<EOF >../postgresql-$V/src/tools/msvc/config.pl
 # Configuration arguments for vcbuild.
 use strict;
 use warnings;
@@ -56,12 +56,14 @@ EOF
 	cp osgeo4w/lib/zlib.lib osgeo4w/lib/zdll.lib
 	cp osgeo4w/lib/iconv.dll.lib osgeo4w/lib/iconv.lib
 
-	vs2019env
 	fetchenv perl/portableshell.bat /SETENV
+	vs2019env
 
-	cd ../src/tools/msvc
-	cmd /c build.bat libpq psql
-	cmd /c install.bat $(cygpath -aw $OSGEO4W_PWD/install) client
+	export PATH="$PATH:/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/amd64/"
+
+	cd ../postgresql-$V/src/tools/msvc
+	cmd /c build.bat >libpq.log 2>&1
+	cmd /c install.bat $(cygpath -aw $OSGEO4W_PWD/install) client || true
 
 	# clean empty directories
 	find install -depth -type d -exec rmdir {} \; 2>/dev/null || true
@@ -73,8 +75,8 @@ export R=$OSGEO4W_REP/x86_64/release/$P
 mkdir -p $R/$P-devel
 
 cat <<EOF >$R/setup.hint
-sdesc: "The libpq library for accessing PostgreSQL + psql commandline client"
-ldesc: "The libpq library for accessing PostgreSQL + psql commandline client"
+sdesc: "The libpq library for accessing PostgreSQL and command line clients"
+ldesc: "The libpq library for accessing PostgreSQL + psql + pg_dump + pg_restore"
 maintainer: $MAINTAINER
 category: Libs Commandline_Utilities
 requires: msvcrt2019 openssl libiconv zlib
@@ -92,7 +94,10 @@ EOF
 
 tar -cjf $R/$P-$V-$B.tar.bz2 \
 	bin/libpq.dll \
-	bin/psql.exe
+	bin/psql.exe \
+	bin/pg_dump.exe \
+	bin/pg_dumpall.exe \
+	bin/pg_restore.exe
 
 tar -cjf $R/$P-devel/$P-devel-$V-$B.tar.bz2 \
 	--exclude include/server \
