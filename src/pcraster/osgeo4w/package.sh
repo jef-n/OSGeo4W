2eee5ed80c9a48d710342ca52453ae474200d4e3
Juergen E. Fischer <jef@norbit.de>
2022-10-13 10:46:16 +0200

    update pcraster to 4.4.0

--- src/pcraster/osgeo4w/package.sh
+++ src/pcraster/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=pcraster
-export V=4.3.3
+export V=4.4.0
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="gdal gdal-devel python3-core python3-devel python3-numpy python3-pybind11 xerces-c-devel qt5-devel qt5-oci qt5-oci-debug boost-devel"
@@ -8,15 +8,13 @@ source ../../../scripts/build-helpers
 
 startlog
 
-if [ -d ../$P-$V ]; then
-	cd ../$P-$V
-	git pull
-	git submodule update --recursive
-else
-	rm -fr build
-	git clone --recurse-submodules https://github.com/$P/$P.git --branch v$V --single-branch ../$P-$V
-	cd ../$P-$V
-fi
+[ -f $P-$V.tar.bz2 ] || wget https://pcraster.geo.uu.nl/$P/packages/src/$P-$V.tar.bz2
+[ -d ../$P-$V ] || tar -C .. -xjf $P-$V.tar.bz2
+[ -f ../$P-$V/patched ] || {
+	patch -d ../$P-$V -p1 --dry-run <patch
+	patch -d ../$P-$V -p1 <patch
+	touch ../$P-$V/patched
+}
 
 cd ../osgeo4w
 
@@ -40,57 +38,34 @@ cd ../osgeo4w
 
 	export LIB="$(cygpath -aw ../osgeo4w/apps/Python39/Libs);$LIB"
 
-	export INCLUDE="$(cygpath -aw ../osgeo4w/include);$INCLUDE"
+	export INCLUDE="$(cygpath -aw ../osgeo4w/include);$(cygpath -aw ../osgeo4w/include/boost-1_74);$INCLUDE"
 	export LIB="$(cygpath -aw ../osgeo4w/lib);$LIB"
 
 	export PATH="$(cygpath -a ../osgeo4w/bin):$(cygpath -a ../osgeo4w/apps/qt5/bin):$PATH"
 
-	for i in $(seq 3); do
-		if ! cmake -G Ninja \
-			-Wno-dev \
-			-D CMAKE_MODULE_PATH=$(cygpath -am ../osgeo4w/share/cmake) \
-			-D CMAKE_PREFIX_PATH=$(cygpath -am ../osgeo4w/apps/qt5/lib/cmake) \
-			-D CMAKE_TOOLCHAIN_FILE=$(cygpath -am ../msvs2019.cmake) \
-			-D Boost_USE_STATIC_LIBS=ON \
-			-D Boost_USE_MULTITHREADED=ON \
-			-D Boost_USE_STATIC_RUNTIME=OFF \
-			-D Boost_INCLUDE_DIR="$(cygpath -am ../osgeo4w/include/boost-1_74)" \
-			-D Boost_LIBRARY_DIR="$(cygpath -am ../osgeo4w/lib)" \
-			-D CMAKE_CXX_STANDARD=17 \
-			-D CMAKE_BUILD_TYPE=Release \
-			-D CMAKE_INSTALL_PREFIX=../install \
-			-D PCRASTER_PYTHON_INSTALL_DIR=$(cygpath -am ../install/apps/Python39/Lib/site-packages) \
-			-D XercesC_INCLUDE_DIR=$(cygpath -am ../osgeo4w/include) \
-			-D XercesC_LIBRARY=$(cygpath -am ../osgeo4w/lib/xerces-c_3.lib) \
-			-D Python3_EXECUTABLE=$(cygpath -am ../osgeo4w/bin/python.exe) \
-			-D pybind11_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/share/cmake/pybind11) \
-			-D PYBIND11_SYSTEM_INCLUDE=$(cygpath -aw ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/include) \
-			-D Python3_NumPy_INCLUDE_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/numpy/core/include) \
-			-D Qt5_DIR=$(cygpath -am ../osgeo4w/apps/Qt5) \
-			-D PCRASTER_BUILD_TEST=OFF \
-			../../$P-$V
-		then
-			patched=0
-			for devbase in _deps/devbase-src _deps/fern-src/devbase; do
-				[ -d $devbase ]
-
-				if ! [ -f $devbase/patched ]; then
-					patch --dry-run -p1 -d $devbase <../devbase.patch
-					patch           -p1 -d $devbase <../devbase.patch
-					touch $devbase/patched
-				fi
-
-				(( patched++ )) || true
-			done
-
-			(( patched == 2 ))
-
-			continue
-		fi
-
-		touch ../../$P-$V/CMakeLists.txt
-		break
-	done
+	cmake -G Ninja \
+		-Wno-dev \
+		-D CMAKE_MODULE_PATH=$(cygpath -am ../osgeo4w/share/cmake) \
+		-D CMAKE_PREFIX_PATH=$(cygpath -am ../osgeo4w/apps/qt5/lib/cmake) \
+		-D CMAKE_TOOLCHAIN_FILE=$(cygpath -am ../msvs2019.cmake) \
+		-D Boost_USE_STATIC_LIBS=ON \
+		-D Boost_USE_MULTITHREADED=ON \
+		-D Boost_USE_STATIC_RUNTIME=OFF \
+		-D Boost_INCLUDE_DIR="$(cygpath -am ../osgeo4w/include/boost-1_74)" \
+		-D Boost_LIBRARY_DIR="$(cygpath -am ../osgeo4w/lib)" \
+		-D CMAKE_CXX_STANDARD=17 \
+		-D CMAKE_BUILD_TYPE=Release \
+		-D CMAKE_INSTALL_PREFIX=../install \
+		-D PCRASTER_PYTHON_INSTALL_DIR=$(cygpath -am ../install/apps/Python39/Lib/site-packages) \
+		-D XercesC_INCLUDE_DIR=$(cygpath -am ../osgeo4w/include) \
+		-D XercesC_LIBRARY=$(cygpath -am ../osgeo4w/lib/xerces-c_3.lib) \
+		-D Python3_EXECUTABLE=$(cygpath -am ../osgeo4w/bin/python.exe) \
+		-D pybind11_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/share/cmake/pybind11) \
+		-D PYBIND11_SYSTEM_INCLUDE=$(cygpath -aw ../osgeo4w/apps/Python39/Lib/site-packages/pybind11/include) \
+		-D Python3_NumPy_INCLUDE_DIR=$(cygpath -am ../osgeo4w/apps/Python39/Lib/site-packages/numpy/core/include) \
+		-D Qt5_DIR=$(cygpath -am ../osgeo4w/apps/Qt5) \
+		-D PCRASTER_BUILD_TEST=OFF \
+		../../$P-$V
 
 	cmake --build .
 	cmake --install . || cmake --install .
@@ -111,8 +86,7 @@ cp ../$P-$V/LICENSE $R/$P-$V-$B.txt
 
 tar -C .. -cjf $R/$P-$V-$B-src.tar.bz2 \
 	osgeo4w/package.sh \
-	osgeo4w/msvs2019.cmake \
-	osgeo4w/devbase.patch
+	osgeo4w/msvs2019.cmake
 
 tar -C install -cjf $R/$P-$V-$B.tar.bz2 \
 	bin \
