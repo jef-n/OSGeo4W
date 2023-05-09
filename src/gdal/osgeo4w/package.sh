7ec4e617efe52f2bac7208ab5c0daa4650247e68
Juergen E. Fischer <jef@norbit.de>
2023-05-09 15:47:53 +0200

    gdal: update to 3.7.0

--- src/gdal/osgeo4w/package.sh
+++ src/gdal/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=gdal
-export V=3.6.4
+export V=3.7.0
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="python3-core swig zlib-devel proj-devel libpng-devel curl-devel geos-devel libmysql-devel sqlite3-devel netcdf-devel libpq-devel expat-devel xerces-c-devel szip-devel hdf4-devel hdf5-devel hdf5-tools ogdi-devel libiconv-devel openjpeg-devel libspatialite-devel freexl-devel libkml-devel xz-devel zstd-devel msodbcsql-devel poppler-devel libwebp-devel oci-devel openfyba-devel freetype-devel python3-devel python3-numpy libjpeg-turbo-devel python3-setuptools opencl-devel libtiff-devel libgeotiff-devel arrow-cpp-devel lz4-devel openssl-devel tiledb-devel lerc-devel kealib-devel odbc-cpp-wrapper-devel"
