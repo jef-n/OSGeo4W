e596975b5ea8a13945b64c0f55e05e07ffba17b2
Juergen E. Fischer <jef@norbit.de>
2023-06-09 17:24:55 +0200

    * python3-pandas: update to 2.0.2 * python3-pytz: update to 2023.3 * add python3-tzdata

--- src/python3-pandas/osgeo4w/package.sh
+++ src/python3-pandas/osgeo4w/package.sh
@@ -2,11 +2,15 @@ export P=python3-pandas
 export V=pip
 export B=pip
 export MAINTAINER=JuergenFischer
-export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-numpy python3-pytz python3-python-dateutil"
+export BUILDDEPENDS="python3-pip python3-wheel python3-devel python3-setuptools python3-numpy python3-pytz python3-python-dateutil"
 
 source ../../../scripts/build-helpers
 
 startlog
+
+fetchenv osgeo4w/bin/o4w_env.bat
+
+pip install Cython versioneer[toml]
 
 packagewheel
 
