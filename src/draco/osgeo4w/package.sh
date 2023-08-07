0aae901f25a5b9e101aa194d73a1ab88bff546e5
Juergen E. Fischer <jef@norbit.de>
2023-08-07 13:18:00 +0200

    add draco 1.5.6 and enable it in qgis-dev (followup 5fbc2015ca2cf947e511eaad7d4d2c4643d0c159; refs qgis/QGIS#54094)

---
+++ src/draco/osgeo4w/package.sh
@@ -0,0 +1,71 @@
+export P=draco
+export V=1.5.6
+export B=next
+export MAINTAINER=JuergenFischer
+export BUILDDEPENDS="python3-core"
+
+source ../../../scripts/build-helpers
+
+startlog
+
+[ -f $P-$V.tar.gz ] || wget -O $P-$V.tar.gz https://github.com/google/$P/archive/refs/tags/$V.tar.gz
+[ -f ../$P-$V/CMakeLists.txt ] || tar -C .. -xzf $P-$V.tar.gz
+if ! [ -f ../$P-$V/patched ]; then
+	patch -p1 -d ../$P-$V --dry-run <patch
+	patch -p1 -d ../$P-$V <patch
+	touch  ../$P-$V/patched
+fi
+
+(
+	set -e
+
+	vs2019env
+	cmakeenv
+	ninjaenv
+
+	mkdir -p build install
+	cd build
+
+	export LIB="$(cygpath -aw ../osgeo4w/lib);$LIB"
+	export INCLUDE="$(cygpath -aw ../osgeo4w/include);$INCLUDE"
+
+	cmake -G Ninja \
+		-D CMAKE_BUILD_TYPE=Release \
+		-D CMAKE_INSTALL_PREFIX=../install \
+		-D PYTHON_EXECUTABLE=$(cygpath -am ../osgeo4w/bin/python3.exe) \
+		../../$P-$V
+	cmake --build .
+	cmake --build . --target install || cmake --build . --target install
+)
+
+export R=$OSGEO4W_REP/x86_64/release/$P
+mkdir -p $R/$P-devel
+
+cat <<EOF >$R/setup.hint
+sdesc: "draco: 3D data compression (executables)"
+ldesc: "Draco is a library for compressing and decompressing 3D geometric meshes and point clouds. It is intended to improve the storage and transmission of 3D graphics."
+category: Commandline_Utilities
+requires:
+category: Libs
+requires: msvcrt2019
+maintainer: $MAINTAINER
+EOF
+
+tar -C install -cjf $R/$P-$V-$B.tar.bz2 bin
+cp ../$P-$V/LICENSE $R/$P-$V-$B.txt
+
+cat <<EOF >$R/$P-devel/setup.hint
+sdesc: "draco: 3D data compression (development)"
+ldesc: "Draco is a library for compressing and decompressing 3D geometric meshes and point clouds. It is intended to improve the storage and transmission of 3D graphics."
+category: Libs
+requires:
+maintainer: $MAINTAINER
+external-source: $P
+EOF
+
+tar -C install -cjf $R/$P-devel/$P-devel-$V-$B.tar.bz2 include lib share
+cp ../$P-$V/LICENSE $R/$P-devel/$P-devel-$V-$B.txt
+
+tar -C .. -cjf $R/$P-$V-$B-src.tar.bz2 osgeo4w/package.sh osgeo4w/patch
+
+endlog
