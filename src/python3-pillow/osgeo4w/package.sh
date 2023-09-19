1848efd6d12f882354fe3c71cd7aab5ef3ea041c
Juergen E. Fischer <jef@norbit.de>
2023-09-19 09:30:04 +0200

    python3-pillow: update to 10.0.1 and build with freetype

--- src/python3-pillow/osgeo4w/package.sh
+++ src/python3-pillow/osgeo4w/package.sh
@@ -2,7 +2,7 @@ export P=python3-pillow
 export V=pip
 export B=pip
 export MAINTAINER=JuergenFischer
-export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools zlib-devel libjpeg-turbo-devel libtiff-devel"
+export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools zlib-devel libjpeg-turbo-devel libtiff-devel freetype-devel"
 
 source ../../../scripts/build-helpers
 
@@ -11,6 +11,6 @@ startlog
 export INCLUDE="$(cygpath -aw osgeo4w/include);$INCLUDE"
 export LIB="$(cygpath -aw osgeo4w/lib);$LIB"
 
-adddepends="zlib libjpeg-turbo libtiff" packagewheel
+adddepends="zlib libjpeg-turbo libtiff freetype" packagewheel
 
 endlog
