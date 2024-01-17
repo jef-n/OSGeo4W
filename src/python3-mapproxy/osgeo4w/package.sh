f6b9748fde4b623fb085d662335ea03a9515ae3f
Juergen E. Fischer <jef@norbit.de>
2024-01-18 00:47:01 +0100

    update python3-mapproxy to 2.0.2

--- src/python3-mapproxy/osgeo4w/package.sh
+++ src/python3-mapproxy/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=python3-mapproxy
-export V=1.16.0
+export V=2.0.2
 export B=pip
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-pyproj"
