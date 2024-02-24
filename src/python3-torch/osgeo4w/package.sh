899853f17aced67d1b15f335355342104174f6bb
Juergen E. Fischer <jef@norbit.de>
2024-02-24 21:51:43 +0100

    python3-torch: update to 2.2.1 (partly fixes #816)

--- src/python3-torch/osgeo4w/package.sh
+++ src/python3-torch/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=python3-torch
-export V=2.0.1
+export V=2.2.1
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-mpmath python3-typing-extensions python3-sympy python3-networkx python3-markupsafe python3-filelock python3-jinja2"
@@ -8,8 +8,8 @@ source ../../../scripts/build-helpers
 
 startlog
 
-p=torch-2.0.1-cp39-cp39-win_amd64.whl
-curl -LO https://files.pythonhosted.org/packages/48/f4/d0b61525a3d3db78636f1937d1bc24cbb39abc57484a545b72b6ab35c114/$p
+p=torch-$V-cp39-cp39-win_amd64.whl
+curl -LO https://files.pythonhosted.org/packages/66/e1/89648ae8f4cb60bb42b59dd991a601abb15a5d1af7ed7dc5989e88e5d3a8/$p
 wheel=$p packagewheel
 
 endlog
