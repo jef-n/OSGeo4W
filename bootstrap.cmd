61349ffd069acb51622860542ad86dd756728264
Juergen E. Fischer <jef@norbit.de>
2024-05-17 17:14:29 +0200

    include perl-YAML-Tiny in cygwin

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -7,7 +7,7 @@ scripts\setup-x86_64.exe ^
 	-R %CD%/cygwin ^
 	-s http://cygwin.mirror.constant.com ^
 	-l %TEMP%/cygwin ^
-	-P "bison,flex,poppler,doxygen,git,unzip,tar,diffutils,patch,curl,wget,flip,p7zip,make,osslsigncode,mingw64-x86_64-gcc-core,catdoc,enscript,mingw64-x86_64-binutils,perl-Data-UUID,ruby=2.6.4-1"
+	-P "bison,flex,poppler,doxygen,git,unzip,tar,diffutils,patch,curl,wget,flip,p7zip,make,osslsigncode,mingw64-x86_64-gcc-core,catdoc,enscript,mingw64-x86_64-binutils,perl-Data-UUID,ruby=2.6.4-1,perl-YAML-Tiny"
 if defined CI echo ::endgroup::
 
 copy bootstrap.sh cygwin\tmp
