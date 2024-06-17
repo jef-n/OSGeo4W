d6d2c59396db0dbd95094ef0b0e3cb2aadfaaea7
Juergen E. Fischer <jef@norbit.de>
2024-06-17 15:18:48 +0200

    rebuild python3-fiona

--- src/python3-fiona/osgeo4w/package.sh
+++ src/python3-fiona/osgeo4w/package.sh
@@ -1,6 +1,6 @@
 export P=python3-fiona
-export V=pip
-export B=pip
+export V=1.9.6
+export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="python3-pip python3-wheel python3-devel python3-setuptools python3-attrs python3-click python3-cligj python3-click-plugins python3-six python3-munch gdal-devel python3-importlib-metadata python3-certifi"
 export PACKAGES="python3-fiona"
@@ -9,6 +9,11 @@ source ../../../scripts/build-helpers
 
 startlog
 
+p=${P#python3-}
+
+[ -f $p-$V.tar.gz ] || wget https://files.pythonhosted.org/packages/92/2c/2ab81e38aad8cf4f4e2a7b1c0488750ffc512c108fb244a8b4dd4f196cd3/$p-$V.tar.gz
+[ -d ../$p-$V ] || tar -C .. -xzf $p-$V.tar.gz
+
 major=$(sed -ne "s/# *define *GDAL_VERSION_MAJOR *//p" osgeo4w/include/gdal_version.h)
 minor=$(sed -ne "s/# *define *GDAL_VERSION_MINOR *//p" osgeo4w/include/gdal_version.h)
 rev=$(sed -ne "s/# *define *GDAL_VERSION_REV *//p" osgeo4w/include/gdal_version.h)
@@ -16,14 +21,14 @@ major=${major%}
 minor=${minor%}
 rev=${rev%}
 
-cat <<EOF >pip.env
 export GDAL_VERSION=$major.$minor.$rev
 export INCLUDE="$(cygpath -am osgeo4w/include);\$INCLUDE"
 export LINK="$(cygpath -am osgeo4w/lib/gdal_i.lib)"
-EOF
 
 fetchenv osgeo4w/bin/o4w_env.bat
 
-adddepends="$RUNTIMEDEPENDS" packagewheel --only-binary Cython
+pip3 install ../$p-$V
+
+adddepends="$RUNTIMEDEPENDS" packagewheel
 
 endlog
