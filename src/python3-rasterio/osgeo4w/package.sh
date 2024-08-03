c9c406eef0b1db11b8849e4ec2b6c0dee024f4f3
Juergen E. Fischer <jef@norbit.de>
2024-08-03 14:27:58 +0200

    update rasterio to 1.3.10

--- src/python3-rasterio/osgeo4w/package.sh
+++ src/python3-rasterio/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=python3-rasterio
-export V=1.3.9
+export V=1.3.10
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="python3-pip python3-wheel python3-devel python3-setuptools python3-affine python3-attrs python3-click python3-cligj python3-numpy python3-snuggs python3-click-plugins gdal-devel python3-certifi"
@@ -11,7 +11,7 @@ startlog
 
 p=${P#python3-}
 
-[ -f $p-$V.tar.gz ] || wget https://files.pythonhosted.org/packages/bd/b8/84f5e6ee1d7915d20ceaa7dbbf2589787c5819907b75c4f2b95386f88961/$p-$V.tar.gz
+[ -f $p-$V.tar.gz ] || wget https://files.pythonhosted.org/packages/26/05/f7c3ee93f270fbd77f7a2d58f2333a21fdf3ef9526a8f0b2a4d5d3d83184/$p-$V.tar.gz
 [ -d ../$p-$V ] || tar -C .. -xzf $p-$V.tar.gz
 
 major=$(sed -ne "s/# *define *GDAL_VERSION_MAJOR *//p" osgeo4w/include/gdal_version.h)
