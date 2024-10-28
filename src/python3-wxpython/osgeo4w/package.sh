3550d54317d3d98bfc7636eac71482ca887b9ea9
Juergen E. Fischer <jef@norbit.de>
2024-10-28 01:31:39 +0100

    python3-wxpython: update to 4.2.2

--- src/python3-wxpython/osgeo4w/package.sh
+++ src/python3-wxpython/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=python3-wxpython
-export V=4.2.1
+export V=4.2.2
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="base python3-core python3-setuptools python3-wheel python3-devel python3-pip python3-attrdict3 wxwidgets-devel python3-numpy python3-pillow python3-six"
@@ -9,7 +9,7 @@ source ../../../scripts/build-helpers
 
 startlog
 
-[ -f wxPython-$V.tar.gz ] || wget https://files.pythonhosted.org/packages/aa/64/d749e767a8ce7bdc3d533334e03bb1106fc4e4803d16f931fada9007ee13/wxPython-$V.tar.gz
+[ -f wxPython-$V.tar.gz ] || wget https://files.pythonhosted.org/packages/a4/f5/8c272764770f47fd419cc2eff4c4fa1c0681c71bcc2f3158b3a83d1339ff/wxPython-$V.tar.gz
 [ -f ../wxPython-$V/setup.py ] || {
 	tar -C .. -xzf wxPython-$V.tar.gz
 	rm -f patched
@@ -30,7 +30,7 @@ startlog
 
 	export INCLUDE="$(cygpath -aw ../osgeo4w/osgeo4w/lib/vc_x64_dll/mswu);$(cygpath -aw ../osgeo4w/osgeo4w/include);$INCLUDE"
 	export LIB="$(cygpath -aw ../osgeo4w/osgeo4w/lib/vc_x64_dll);$LIB"
-	python3 build.py build_py --release --x64 --use_syswx --extra_waf='--msvc_version="msvc 17.9"'
+	python3 build.py build_py --release --x64 --use_syswx --extra_waf='--msvc_version="msvc 17.8"'
 
 	python3 build.py bdist_wheel
 
