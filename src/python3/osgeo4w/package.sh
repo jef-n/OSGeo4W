bcb5da53148869ee82c3a887fefe5db883f693fb
Juergen E. Fischer <jef@norbit.de>
2025-10-15 00:47:43 +0200

    update python to 3.12.12

--- src/python3/osgeo4w/package.sh
+++ src/python3/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=python3
-export V=3.12.11
+export V=3.12.12
 export B="next $P-core"
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="openssl-devel bzip2-devel xz-devel zlib-devel sqlite3-devel"
