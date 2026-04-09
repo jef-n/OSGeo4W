27ed0376e35e53629b893124fd5ce0b48cf85f64
Juergen E. Fischer <jef@norbit.de>
2026-04-09 18:52:24 +0200

    zlib: update to 1.3.2 (patched to keep zlib.dll and .lib name)

--- src/zlib/osgeo4w/package.sh
+++ src/zlib/osgeo4w/package.sh
@@ -1,5 +1,5 @@
 export P=zlib
-export V=1.3.1
+export V=1.3.2
 export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS=none
@@ -68,28 +68,8 @@ EOF
 
 tar -C install -cjf $R/$P-devel/$P-devel-$V-$B.tar.bz2 include lib share
 
-cat <<EOF | tee -a $R/$P-$V-$B.txt >$R/$P-devel/$P-devel-$P-$V-$B.txt
-  Copyright (C) 1995-2017 Jean-loup Gailly and Mark Adler
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-
-  Jean-loup Gailly        Mark Adler
-  jloup@gzip.org          madler@alumni.caltech.edu
-EOF
+cp ../$P-$V/LICENSE $R/$P-$V-$B.txt
+cp ../$P-$V/LICENSE $R/$P-devel/$P-devel-$V-$B.txt
 
 tar -C .. -cjf $R/$P-$V-$B-src.tar.bz2 osgeo4w/package.sh osgeo4w/patch
 
