cc45ae4b546892b0b6f3fda8dd3c5bfe8e5c8a0a
Juergen E. Fischer <jef@norbit.de>
2026-04-14 15:37:21 +0200

    python3-pandas: trim dependencies

--- src/python3-pandas/osgeo4w/package.sh
+++ src/python3-pandas/osgeo4w/package.sh
@@ -2,7 +2,7 @@ export P=python3-pandas
 export V=pip
 export B=pip
 export MAINTAINER=JuergenFischer
-export BUILDDEPENDS="python3-pip python3-wheel python3-devel python3-setuptools python3-numpy python3-pytz python3-python-dateutil python3-tzdata"
+export BUILDDEPENDS="python3-pip python3-wheel python3-devel python3-setuptools python3-numpy python3-python-dateutil python3-tzdata"
 export PACKAGES="python3-pandas"
 
 source ../../../scripts/build-helpers
