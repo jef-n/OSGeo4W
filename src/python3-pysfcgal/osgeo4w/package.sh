eb9126b1518725cfc975ec4c749dfc77cde30589
Juergen E. Fischer <jef@norbit.de>
2026-05-15 19:33:11 +0200

    python3-pysfcgal: build from source

--- src/python3-pysfcgal/osgeo4w/package.sh
+++ src/python3-pysfcgal/osgeo4w/package.sh
@@ -2,13 +2,16 @@ export P=python3-pysfcgal
 export V=pip
 export B=pip
 export MAINTAINER=JeanFelder
-export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-devel python3-cffi sfcgal"
+export BUILDDEPENDS="python3-pip python3-wheel python3-setuptools python3-devel python3-cffi sfcgal-devel"
 export PACKAGES="python3-pysfcgal"
 
 source ../../../scripts/build-helpers
 
 startlog
 
-packagewheel
+export LIB="$(cygpath -am osgeo4w/lib);$LIB"
+export INCLUDE="$(cygpath -am osgeo4w/include);$INCLUDE"
+
+OSGEO4W_PY_INCLUDE_BINARY=1 PIP_NO_BINARY=pysfcgal adddepends=sfcgal packagewheel
 
 endlog
